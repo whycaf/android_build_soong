8590fb4a8ec0fc9e12fdd0571ff82420fcda6e2d
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with kcuf board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of KCUF variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"kcuf/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,9 @@ type variableProperties struct {
 		Device_support_hwfde_perf struct {
 			Cflags []string
 		}
+
+		// include Kcuf variables
+		Kcuf android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -242,6 +247,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Kcuf variables
+	Kcuf android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
